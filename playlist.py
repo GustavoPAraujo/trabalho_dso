5e7e81dabe2aa44a687c37e51610ace9beac9c37
Drumond <guidrumond2409@gmail.com>
2024-05-04 17:26:10 -0300

    mudancas_playlist

--- playlist.py
+++ playlist.py
@@ -3,5 +3,16 @@ from usuario import Usuario
 
 
 class PlayList:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, musica: Musica, nome_playlist):
+        self.__playlist = list()
+        self.__nome_playlist = nome_playlist
+
+    @property
+    def nome_playlist(self):
+        return self.__nome_playlist
+    
+    @nome_playlist.setter
+    def nome_playlist(self, nome_playlist):
+        self.__nome_playlist = nome_playlist
+    
+    
