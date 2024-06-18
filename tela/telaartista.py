34ecfe7fa0ae95aa2a11186af6e7ed4a27c9e644
GustavoPAraujo <araujops.gustavo@gmail.com>
2024-06-18 14:36:32 -0300

    controlador e tela artista

--- tela/telaartista.py
+++ tela/telaartista.py
@@ -9,34 +9,36 @@ class TelaArtista:
         sg.theme('Reddit')
         layout = [
             [sg.Text('Artista')],
-            [sg.Radio('cadastra_usuario', "RADIO1", key='1')],
-            [sg.Radio('cadastra_musica', "RADIO1", key='2')],
-            [sg.Radio('cadastra_artista', "RADIO1", key='3')],
-            [sg.Radio('cadastra_genero', "RADIO1", key='4')],
-            [sg.Radio('cadastra_playlist', "RADIO1", key='5')],
-            [sg.Radio('gera_recomendacao', "RADIO1", key='6')],
-            [sg.Radio('encerra_sistema', "RADIO1", key='7')],
+            [sg.Radio('Cadastrar Artista', "RADIO1", key='1')],
+            [sg.Radio('Voltar', "RADIO1", key='2')],
             [sg.Ok(), sg.Cancel()]
         ]
         self.__window = sg.Window('Tela do Sistema', layout)
 
-'''
-class TelaArtista:
-
     def tela_opcoes(self):
-        print("_____Artista_____")
-        print("1: Cadastrar Artista")
-        print("0: RETORNAR")
-        while True:
-            try:
-                opcao = int(input("Escolha uma opção: "))
-                if  0 <= opcao <= 1:
-                    return opcao
-                else:
-                    print("\nEscolha uma opção válida\n")
-            except ValueError:
-                print("\nPor favor, insira um número inteiro.\n")
-    
+        self.init_components()
+        button, values = self.__window.read() # type: ignore
+        opcao = 0
+
+        if values:
+            for key in values:
+                if values[key]:
+                    opcao = int(key)
+
+        if button in (None, 'Cancel'):
+            opcao = 0
+        
+        self.close()
+        return opcao
+
+    def close(self):
+        if self.__window:
+            self.__window.close()
+            self.__window = None
+
+
+#fazer
+'''
     def criar_artista(self):
         print("_____Dados_do_Artista_____")
         nome = input("Digite o nome: ")
