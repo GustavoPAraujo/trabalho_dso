71031d83452107d739a2eaa4ae148e265022437b
GustavoPAraujo <araujops.gustavo@gmail.com>
2024-06-18 15:10:57 -0300

    tela e controlador playlist

--- tela/telaplaylist.py
+++ tela/telaplaylist.py
@@ -1,6 +1,25 @@
-
+import PySimpleGUI as sg
 
 class TelaPlaylist:
+    def __init__(self):
+        self.__window = None
+        self.init_components()
+
+    def init_components(self):
+        sg.theme('Reddit')
+        layout = [
+            [sg.Text('Tela do Sistema')],
+            [sg.Radio('Criar Playlist', "RADIO1", key='1')],
+            [sg.Radio('Excluir Playlist', "RADIO1", key='2')],
+            [sg.Radio('Selecionar Playlist', "RADIO1", key='3')],
+            [sg.Radio('Alterar nome da Playlist', "RADIO1", key='4')],
+            [sg.Radio('adicionar música', "RADIO1", key='5')],
+            [sg.Radio('Excluir música', "RADIO1", key='6')],
+            [sg.Radio('Voltar', "RADIO1", key='7')],
+            [sg.Ok(), sg.Cancel()]
+        ]
+        self.__window = sg.Window('Tela do Sistema', layout)
+
     def tela_opcoes(self):
         print('___Tela Do Sistema___')
         print('1: Criar Playlist')
@@ -20,7 +39,9 @@ class TelaPlaylist:
                     print("\nEscolha uma opção válida\n")
             except ValueError:
                 print("\nPor favor, insira um número inteiro.\n")
-        
+
+#fazer
+'''
     def pega_nome_playlist(self):
         print("_____Escolha nome da Playlist_____")
         nome_playlist = input("Nome da Playlist ")
@@ -44,3 +65,4 @@ class TelaPlaylist:
         print(nome)
         musicas = dados_playlist['musicas']
         print(musicas)
+'''
