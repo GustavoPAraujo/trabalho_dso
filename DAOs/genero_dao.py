b6ad1be4c57fce27974cf21161df8f761e329b81
GustavoPAraujo <araujops.gustavo@gmail.com>
2024-06-23 18:16:53 -0300

    genero dao

---
+++ DAOs/genero_dao.py
@@ -0,0 +1,15 @@
+from DAOs.dao import DAO
+from entidades.genero import Genero
+
+class GeneroDAO(DAO):
+    def __init__(self):
+        super().__init__('genero.pkl')
+
+    def add(self):
+        pass
+
+    def get(self):
+        pass
+
+    def remove(self):
+        pass
