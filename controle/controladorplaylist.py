a913f8ae5646387673da5fc849109ce201568eef
Drumond <guidrumond2409@gmail.com>
2024-06-30 20:43:53 -0300

    adicionar musica

--- controle/controladorplaylist.py
+++ controle/controladorplaylist.py
@@ -75,38 +75,34 @@ class ControladorPlaylist:
     #fazer
     def alterar_nome_playlist(self):
         nome_playlist = self.__tela_playlist.pega_nome_playlist()
-        for playlist in self.__lista_playlist:
-            if nome_playlist == playlist.nome_playlist:
-                novo_nome = self.__tela_playlist.alterar_nome_playlist()
-                playlist.nome_playlist = novo_nome
-                print('Nome editado com sucesso')
-            else:
-                print( 'Essa PlayList não existe')
+        playlist = self.__playlist_dao.get(nome_playlist)
+        if playlist is not None:
+            novo_nome = self.__tela_playlist.alterar_nome_playlist()
+            playlist.nome_playlist = novo_nome
+            return
+      
 
     #5
     def adicionar_musica(self):
         nome_playlist = self.__tela_playlist.pega_nome_playlist()
-
-        playlist_encontrada = None
-        for playlist in self.__lista_playlist:
-            if nome_playlist == playlist.nome_playlist:
-                playlist_encontrada = playlist
-                return playlist_encontrada
-            else:
-                print('Essa playlist não existe')
-                return
+        # verificar existencia da playlist
+        playlist = self.__playlist_dao.get(nome_playlist)
+        if playlist is None:
+            self.__tela_playlist.mostra_mnsg('Playlist inexistente')
+            return
             
         dados_musica = self.__tela_playlist.pegar_musica()
         nome_musica = dados_musica['nome_musica']
-        artista = dados_musica['artista']
-        genero = dados_musica['genero']
+        artista = self.__controlador_sistema.controlador_artista.pega_artista_por_nome(dados_musica["artista"])
+        genero = self.__controlador_sistema.controlador_genero.seleciona_genero(dados_musica["genero"])
+        #verifica existencia da nova musica
         musica_verificada = self.__controlador_sistema.controlador_musica.verificar_musica(nome_musica, artista, genero)
-
-        if musica_verificada not in playlist_encontrada.musicas:
-            playlist_encontrada.musicas.append(musica_verificada)
-            print('Música adicionada com sucesso')
+        if musica_verificada is None:
+            self.__tela_playlist.mostra_mnsg('A música não foi encontrada ou não pôde ser verificada.')
+            return
         else:
-            print('Música já presente na playlist')
+            playlist.append(musica_verificada)
+        #adiciona nova musica
         
 
     #6
